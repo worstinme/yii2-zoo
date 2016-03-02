ac787d62518385216732525f53c590099d4766e2
Evgeny Zakirov <box@flyleaf.su>
2016-03-02 22:52:05 +0500

    image element

--- elements/images/_params.php
+++ elements/images/_params.php
@@ -8,5 +8,6 @@ use yii\helpers\Html;
 
 <form>
 <?= Html::checkbox('preview', $checked = false,['label'=>'Отобразить превью первой картинки']); ?><br>
-<?= Html::checkbox('asUrl', $checked = false,['label'=>'Отображать ссылку на материал?']); ?>
+<?= Html::checkbox('asUrl', $checked = false,['label'=>'Отображать ссылку на материал?']); ?><br>
+<?= Html::checkbox('gallery', $checked = false,['label'=>'Отображать в виде галлереи??']); ?>
 </form>
--- elements/images/_view.php
+++ elements/images/_view.php
@@ -1,18 +1,46 @@
 <?php
 
 use yii\helpers\Html; 
+use yii\imagine\Image;
+use Imagine\Image\Box;
 
-$url = isset($params['asUrl']) && $params['asUrl'] == 1 ? true : false;
-$preview = isset($params['preview']) && $params['preview'] == 1 ? true : false;
+worstinme\uikit\assets\Slideset::register($this);
 
-if ($preview) {
+?>
 
-	$image  = Html::img($model->{$attribute}[0]);
+<?php if (isset($params['gallery']) && $params['gallery'] == 1) : ?>
+	
+	<ul id="slider" class="uk-switcher">
+		<?php foreach ($model->{$attribute} as $img_url): ?>
+			<li><?=Html::img($img_url);?></li>
+		<?php endforeach ?>
+	</ul>
 
-	echo $url ? Html::a($image, $model->url) : $image;
+	<div data-uk-slideset="{default: 4}">
+	    <ul class="uk-grid uk-grid-collapse uk-slideset" data-uk-switcher="{connect:'#slider'}">
+	        <?php foreach ($model->{$attribute} as $img_url): ?>
+				<li><?=Html::img($img_url);?></li>
+			<?php endforeach ?>
+	    </ul>
+	    <a href=""data-uk-slideset-item="previous"></a>
+	    <a href=""data-uk-slideset-item="next"></a>
+	</div>
 
-} else {
-	foreach ($model->{$attribute} as $img_url) {
-		echo $url ? Html::a(Html::img($img_url), $model->url) : Html::img($img_url);
+<?php else: 
+
+	$url = isset($params['asUrl']) && $params['asUrl'] == 1 ? true : false;
+	$preview = isset($params['preview']) && $params['preview'] == 1 ? true : false;
+
+	if ($preview) {
+
+		$image  = Html::img($model->{$attribute}[0]);
+
+		echo $url ? Html::a($image, $model->url) : $image;
+
+	} else {
+		foreach ($model->{$attribute} as $img_url) {
+			echo $url ? Html::a(Html::img($img_url), $model->url) : Html::img($img_url);
+		}
 	}
-}
+
+endif;
