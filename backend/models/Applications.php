7b2ccb40033e862a0dfca2a3919645eab73e1e77
Evgeny Zakirov <box@flyleaf.su>
2016-04-23 06:03:05 +0500

    categories state disabled in backend

--- backend/models/Applications.php
+++ backend/models/Applications.php
@@ -54,6 +54,13 @@ class Applications extends \worstinme\zoo\models\Applications
         ];
     }
 
+    public function getParentCategories() {
+        return $this->hasMany(Categories::className(), ['app_id' => 'id'])->where(['parent_id'=>0])->orderBy('sort ASC');
+    }
+
+    public function getCategories() {
+        return $this->hasMany(Categories::className(), ['app_id' => 'id'])->orderBy('sort ASC');
+    }
 
 
     public function setTemplate($name,$template) {
