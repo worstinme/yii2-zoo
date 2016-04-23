c21a155d6636d47a92c9d43a391ceed1dff4ef94
Evgeny Zakirov <box@flyleaf.su>
2016-04-23 06:02:09 +0500

    +lightbox

--- elements/image/_params.php
+++ elements/image/_params.php
@@ -11,4 +11,5 @@ use yii\helpers\Html;
 <?= Html::checkbox('asUrl', isset($params['asUrl']) ? $params['asUrl'] : null,['label'=>'Отображать ссылку на материал?']); ?><br>
 <?= Html::textInput('width', isset($params['width']) ? $params['width'] : null, ['placeholder' => 'Ширина']); ?><br>
 <?= Html::textInput('height', isset($params['height']) ? $params['height'] : null, ['placeholder' => 'Высота']); ?><br>
+<?= Html::checkbox('lightbox', isset($params['lightbox']) ? $params['lightbox'] : null, ['label' => 'lightbox']); ?><br>
 </form>
--- elements/image/_view.php
+++ elements/image/_view.php
@@ -5,6 +5,7 @@ use yii\helpers\Html;
 $width = isset($params['width']) ? (int)$params['width'] : null;
 $height = isset($params['height']) ? (int)$params['height'] : null;
 
+
 if ($width > 0 && $height > 0) {
 	$thumbnail = '/thumbnails/'.$width.'-'.$height.'/';
 }
@@ -13,6 +14,12 @@ if(!empty($model->{$attribute})) {
 
 	$image = !empty($thumbnail)?Html::img($thumbnail.ltrim($model->{$attribute},"/")):Html::img($model->{$attribute});
 
-	echo !empty($params['asUrl']) && $params['asUrl'] ? Html::a($image, $model->url,['data-pjax'=>0]) : $image;
+	if (isset($params['lightbox']) && $params['lightbox']) {
+		\worstinme\uikit\assets\Lightbox::register($this);
+		echo Html::a($image, $model->{$attribute} ,['data'=>['pjax'=>0,'uk-lightbox'=>true]]) ;
+	}
+	else {
+		echo !empty($params['asUrl']) && $params['asUrl'] ? Html::a($image, $model->url,['data-pjax'=>0]) : $image;
+	}
 
 }
