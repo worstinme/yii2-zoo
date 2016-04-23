e8664eb7b6d79bbc54ee242cbe9bf4a0a4ee5ac0
Evgeny Zakirov <box@flyleaf.su>
2016-04-23 06:00:44 +0500

    shortcodes parser & config getter

--- Zoo.php
+++ Zoo.php
@@ -5,12 +5,15 @@ namespace worstinme\zoo;
 use Yii;
 use yii\helpers\FileHelper;
 use worstinme\zoo\models\Applications;
+use worstinme\zoo\backend\models\Config;
 
 class Zoo extends \yii\base\Component { 
 
 	public $appsConfigPath = '@app/config/zoo';
 	public $elementsPath = '@app/components/elements';
 	public $renderersPath = '@app/components/renderers';
+
+	public $widgets = [];
 
 	private $menu;
 
@@ -23,4 +26,25 @@ class Zoo extends \yii\base\Component {
 
 		return $this->menu;
 	}
+
+	public function findWidgets($content) {
+
+		$shortcode = new \worstinme\zoo\helpers\ShortcodeHelper;
+		$shortcode->callbacks = array_merge([
+			'uk-slideshow'=>['worstinme\uikit\widgets\Slideshow','widget'],
+			//'anothershortcode'=>function($attrs, $content, $tag){},
+		],$this->widgets);
+
+		return $shortcode->parse($content);
+	}
+
+	public function config($name, $default = null) {
+
+		if (($config = Config::find()->where(['name'=>$name])->one()) !== null) {
+			return $config->value;
+		}
+
+		return $default;
+	}
+
 }
