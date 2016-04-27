426f84d76214233cbe2b312585c1f45e38a20167
Evgeny Zakirov <box@flyleaf.su>
2016-04-27 13:29:50 +0500

    hide without categories

--- elements/category/_form.php
+++ elements/category/_form.php
@@ -5,41 +5,39 @@ use yii\helpers\ArrayHelper;
 
 $this->registerJs($model->addValidators($this,$attribute), 5);
 
-$parent_categories = ArrayHelper::map(Yii::$app->controller->app->parentCategories,'id','name');
-
-
 $multiselect = isset($params['multiselect']) ? (int)$params['multiselect'] : 0;
 
 ?>
 
-<?= Html::activeLabel($model, 'category',['class'=>'uk-form-label']); ?>
+<?php if ($multiselect && count($model->app->catlist)): ?>
 
-
-<?php if ($multiselect ): ?>
+	<?= Html::activeLabel($model, 'category',['class'=>'uk-form-label']); ?>
 
 	<?=Html::activeDropDownList($model,$attribute,$model->app->catlist,['class'=>'uk-width-1-1','multiple'=>'multiple','size'=>10])?>
 
-<?php else: ?>
-	
-<div class="uk-form-controls">
-	<?=Html::activeDropDownList($model,$attribute.'[0]',$parent_categories,['id'=>'item-'.$attribute,'prompt'=>'выбрать из списка','class'=>'uk-width-1-1 category-select'])?>
-	<?php if (count($model->category)): ?>
-	<?php $i=0;foreach ($model->category as $key => $category_id): ?>
-		<?php if ($category_id > 0): ?>		
-			<? $related_cats = ArrayHelper::map(\worstinme\zoo\backend\models\Categories::find()->where(['parent_id'=>$category_id])->all(),'id','name'); ?>
-			<?php if (count($related_cats)): $i++; ?>
-			<div class="related-category-select uk-margin-top">
-				<?=Html::activeDropDownList($model,$attribute.'['.($key+1).']',$related_cats,['prompt'=>'выбрать из списка','class'=>'uk-width-1-1  category-select'])?>	
-			<?php endif ?>
-		<?php endif ?>
-	<?php endforeach ?>
+<?php elseif(($parent_categories = ArrayHelper::map(Yii::$app->controller->app->parentCategories,'id','name')) !==null && count($parent_categories)): ?>
 
-	<?php while ($i > 0) {
-		echo '</div>'; $i--;
-	} ?>
+	<?= Html::activeLabel($model, 'category',['class'=>'uk-form-label']); ?>
 		
-	<?php endif ?>
-	<div class="uk-form-help-block uk-text-danger"></div>
-</div>
+	<div class="uk-form-controls">
+		<?=Html::activeDropDownList($model,$attribute.'[0]',$parent_categories,['id'=>'item-'.$attribute,'prompt'=>'выбрать из списка','class'=>'uk-width-1-1 category-select'])?>
+		<?php if (count($model->category)): ?>
+		<?php $i=0;foreach ($model->category as $key => $category_id): ?>
+			<?php if ($category_id > 0): ?>		
+				<? $related_cats = ArrayHelper::map(\worstinme\zoo\backend\models\Categories::find()->where(['parent_id'=>$category_id])->all(),'id','name'); ?>
+				<?php if (count($related_cats)): $i++; ?>
+				<div class="related-category-select uk-margin-top">
+					<?=Html::activeDropDownList($model,$attribute.'['.($key+1).']',$related_cats,['prompt'=>'выбрать из списка','class'=>'uk-width-1-1  category-select'])?>	
+				<?php endif ?>
+			<?php endif ?>
+		<?php endforeach ?>
 
-<?php endif ?>
+		<?php while ($i > 0) {
+			echo '</div>'; $i--;
+		} ?>
+			
+		<?php endif ?>
+		<div class="uk-form-help-block uk-text-danger"></div>
+	</div>
+
+<?php endif; ?>
