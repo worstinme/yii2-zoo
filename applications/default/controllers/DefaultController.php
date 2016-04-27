bf33d35a2144baecec8935d15f1ddd8b282a7012
Evgeny Zakirov <box@flyleaf.su>
2016-04-27 13:00:01 +0500

    commented actions example

--- applications/default/controllers/DefaultController.php
+++ applications/default/controllers/DefaultController.php
@@ -17,5 +17,53 @@ use yii\web\NotFoundHttpException;
 class <?= $controllerName ?> extends \worstinme\zoo\Controller
 {
 
- 
+/*
+    protected function renderApplication($app = null) {
+
+        $app = $app === null ? $this->app : $app;
+
+        $searchModel = new s();
+        $searchModel->app_id = $app->id;
+        $searchModel->regBehaviors();
+
+        $dataProvider = $searchModel->data(Yii::$app->request->queryParams); 
+
+        return $this->render('application', [
+            'app'=>$app,
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+*/
+
+/*
+    protected function renderItem($model) {
+
+        return $this->render('item',[
+            'model'=>$model,
+        ]);
+    }
+*/
+
+/*
+    protected function renderCategory($category) {
+
+        $app = $category->app;
+
+        $searchModel = new s();
+        $searchModel->app_id = $app->id;
+        $searchModel->regBehaviors();
+        $searchModel->category = [$category->id];
+
+        $dataProvider = $searchModel->data(Yii::$app->request->queryParams);
+
+        return $this->render('category',[
+            'category'=>$category,
+            'app'=>$app,
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+
+    }
+*/
 }
