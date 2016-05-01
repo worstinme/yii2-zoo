6cadbaaf1fa9d53cdd4e088528d4bc38f722ed7a
Evgeny Zakirov <box@flyleaf.su>
2016-05-01 19:26:11 +0500

    deprecated

--- backend/Module.php
+++ backend/Module.php
@@ -53,32 +53,6 @@ class Module extends \yii\base\Module
 
     }
 
-    public function getRenderers() {
-
-        $files = AppHelper::findDirectories(Yii::getAlias('@worstinme/zoo/renderers'));
-
-        $files = array_unique(array_merge($files, AppHelper::findDirectories(Yii::getAlias(Yii::$app->zoo->renderersPath))));
-
-        $renderers = [];
-
-        foreach ($files as $file) {
-            $renderers[$file] = Inflector::camel2words($file);
-        }
-
-        return $renderers;
-
-    }
-
-    public function getTemplates() {
-        return [
-            'form',
-            'full',
-            'teaser',
-            'related',
-            'submission',
-        ];
-    }
-
 	public static function t($category, $message, $params = [], $language = null)
 	{
 	    return Yii::t('modules/zoo/' . $category, $message, $params, $language);
