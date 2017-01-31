4a7b73976862992464f8d70aa6cbea3a8d55dd4e
Evgeny Zakirov <box@flyleaf.su>
2017-01-31 16:26:14 +0800

    update

--- widgets/views/last-items.php
+++ widgets/views/last-items.php
@@ -6,10 +6,13 @@ use yii\helpers\Html;
 <?php if (!empty($title)): ?>
     <h3 class="uk-panel-title"><?= Html::encode($title) ?></h3>
 <?php endif ?>
-<ul<?= $list_class ? ' class="' . $list_class . '"' : '' ?>>
-    <?php foreach ($items as $key => $item): ?>
-        <li>
-            <?= \worstinme\zoo\helpers\TemplateHelper::render($item, 'list-item') ?>
-        </li>
-    <?php endforeach ?>
-</ul>
+
+<div class="last-items-list">
+    <ul<?= $list_class ? ' class="' . $list_class . '"' : '' ?>>
+        <?php foreach ($items as $key => $item): ?>
+            <li>
+                <?= \worstinme\zoo\helpers\TemplateHelper::render($item, 'list-item') ?>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</div>
