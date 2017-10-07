1b1909224b35b52cc62837174d29dabeee512ed5
Eugene Zakirov <worstinme@gmail.com>
2017-10-07 15:31:35 +0800

    MBT-36 #comment переключение языка, переключалка между связанными страницами

--- Controller.php
+++ Controller.php
@@ -13,6 +13,7 @@ use yii\web\NotFoundHttpException;
 class Controller extends \yii\web\Controller
 {
     public $app;
+    public $model;
 
     public function init() {
 
@@ -46,9 +47,11 @@ class Controller extends \yii\web\Controller
 
         if (($category = Categories::find()->where(['alias'=>$a,'app_id'=>$this->app->id])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
             // материал дефолтного приложения по алиасу
+            $this->model = $category;
             return $this->renderCategory($category);
         }
         elseif (($model = $this->app->getItems()->where([Items::tablename().'.alias'=>$a])->orWhere([Items::tablename().'.id'=>$a])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
+            $this->model = $model;
             return $this->renderItem($model);
         }
 
@@ -62,15 +65,17 @@ class Controller extends \yii\web\Controller
         if (($parent_cat = Categories::find()->where(['app_id'=>$this->app->id,'alias'=>$a])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
             // приложение -> категория по алиасу
             if (($category = Categories::find()->where(['app_id'=>$this->app->id,'parent_id'=>$parent_cat->id,'alias'=>$b])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
-
+                $this->model = $category;
                 return $this->renderCategory($category);
             }
             elseif(($model = Items::find()->where(['app_id'=>$this->app->id,'alias'=>$b])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
                 // приложение -> материал по алиасу
+                $this->model = $model;
                 return $this->renderItem($model);
             }
             elseif(($model = Items::find()->where(['app_id'=>$this->app->id,'id'=>$b])->one()) !== null) {
                 // приложение -> материал по ID
+                $this->model = $model;
                 return $this->renderItem($model);
             }
         }
@@ -87,15 +92,17 @@ class Controller extends \yii\web\Controller
             if (($parent_cat = Categories::find()->where(['app_id'=>$app->id,'alias'=>$b])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
                 // приложение -> категория по алиасу
                 if (($category = Categories::find()->where(['app_id'=>$app->id,'parent_id'=>$parent_cat->id,'alias'=>$c])->one()) !== null) {
-
+                    $this->model = $category;
                     return $this->renderCategory($category);
                 }
                 elseif(($model = Items::find()->where(['app_id'=>$app->id,'alias'=>$c])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
                     // приложение -> материал по алиасу
+                    $this->model = $model;
                     return $this->renderItem($model);
                 }
                 elseif(($model = Items::find()->where(['app_id'=>$app->id,'id'=>$c])->one()) !== null) {
                     // приложение -> материал по ID
+                    $this->model = $model;
                     return $this->renderItem($model);
                 }
             }
@@ -106,10 +113,12 @@ class Controller extends \yii\web\Controller
             if (($category = Categories::find()->where(['app_id'=>1,'alias'=>$a])->andFilterWhere(['lang'=>$lang])->one()) !== null) {
                 if(($model = Items::find()->where(['app_id'=>1,'alias'=>$b])->andFilterWhere(['lang'=>$lang])->one()) !== null && $model->parentCategory->alias == $a) {
                     // приложение -> материал по алиасу
+                    $this->model = $model;
                     return $this->renderItem($model);
                 }
                 elseif(($model = Items::find()->where(['app_id'=>1,'id'=>$b])->one()) !== null && $model->parentCategory->alias == $a) {
                     // приложение -> материал по ID
+                    $this->model = $model;
                     return $this->renderItem($model);
                 }
             }
@@ -178,5 +187,18 @@ class Controller extends \yii\web\Controller
             'dataProvider' => $dataProvider,
         ]);
 
+    }
+
+    public function getAlternateUrl($lang) {
+        if ($this->model !==null) {
+            if ($this->model instanceof Categories || $this->model instanceof Items) {
+                foreach ($this->model->alternates as $alternate) {
+                    if ($alternate->lang == $lang) {
+                        return $alternate->url;
+                    }
+                }
+            }
+        }
+        return [$this->id.'/index','lang'=>$lang];
     }
 }
