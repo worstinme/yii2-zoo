495fc4f3ed93ac0349d0856cda1138162f2c6415
Eugene Zakirov <worstinme@gmail.com>
2018-02-07 20:47:09 +0800

    frontend items relations fix

--- frontend/models/Items.php
+++ frontend/models/Items.php
@@ -6,15 +6,21 @@ class Items extends \worstinme\zoo\models\Items
 {
     public function getCategories()
     {
-        return parent::getCategories()->where(['state'=>1]);
+        return $this->hasMany(Categories::className(), ['id' => 'category_id'])
+            ->andOnCondition([Categories::tableName().'.state'=>1])
+            ->viaTable('{{%items_categories}}', ['item_id' => 'id']);
     }
 
     public function getParentCategory()
     {
-        return parent::getParentCategory()->where(['state'=>1]);
+        return $this->hasOne(Categories::className(), ['id' => 'parent_category_id'])
+            ->andOnCondition([Categories::tableName().'.state'=>1]);
     }
 
     public function getAlternates() {
-        return parent::getAlternates()->where(['state'=>1]);
+        return $this->hasMany(Items::className(),['id'=>'alternate_id'])
+            ->andOnCondition([Items::tableName().'.state'=>1])
+            ->viaTable('{{%items_alternates}}',['item_id'=>'id']);
     }
+
 }
