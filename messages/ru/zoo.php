191230f52cf804d908f7026f5abd83e40026a4b7
Eugene Zakirov <worstinme@gmail.com>
2018-02-18 15:56:17 +0800

    navigation messages

--- messages/ru/zoo.php
+++ messages/ru/zoo.php
@@ -5,4 +5,12 @@ return [
     'NAV_ELFINDER' => 'Файлы',
     'NAV_MENU' => 'Меню',
     'NAV_CONFIG' => 'Настройки',
+
+    'NAV_APPLICATION_ITEMS'=>'Страницы',
+    'NAV_CREATE_ITEM'=>'Создать страницу',
+    'NAV_APPLICATION_CATEGORIES'=>'Категории',
+    'NAV_CREATE_CATEGORY'=>'Создать категорию',
+    'NAV_APPLICATION_ELEMENTS'=>'Элементы',
+    'NAV_CREATE_ELEMENT'=>'Создать элемент',
+
 ];
