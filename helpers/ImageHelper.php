bfaeec0bcda7e8255787ce791e95cb5c4e8191fa
Eugene Zakirov <worstinme@gmail.com>
2018-03-01 18:09:13 +0800

    blank image src changed

--- helpers/ImageHelper.php
+++ helpers/ImageHelper.php
@@ -56,7 +56,7 @@ class ImageHelper
         $filename = FileHelper::normalizePath(Yii::getAlias($filename));
         if (!is_file($filename)) {
             Yii::error("File $filename doesn't exist, used fish");
-            $filename = FileHelper::normalizePath(Yii::getAlias('@worstinme/zoo/assets/images/zaglushka.jpg'));
+            $filename = FileHelper::normalizePath(Yii::getAlias('@worstinme/zoo/backend/assets/images/zaglushka.jpg'));
         }
         $cachePath = Yii::getAlias('@webroot/' . self::$cacheAlias);
         $thumbnailFileExt = strrchr($filename, '.');
