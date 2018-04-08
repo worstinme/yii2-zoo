9daaf79ceb25b6d28ab12b7f924c74f9853252a3
Eugene Zakirov <worstinme@gmail.com>
2018-04-08 12:02:36 +0800

    navigation fixes

--- messages/ru/zoo.php
+++ messages/ru/zoo.php
@@ -1,17 +1,20 @@
 <?php
 return [
     'SHOW_ALL' => 'Показать все',
-    'NAV_APPLICATIONS' => 'Приложения',
+    'NAV_APPLICATIONS' => 'Разделы',
     'NAV_ELFINDER' => 'Файлы',
     'NAV_MENU' => 'Меню',
     'NAV_CONFIG' => 'Настройки',
 
     'NAV_APPLICATION_ITEMS'=>'Страницы',
+    'NAV_APPLICATION_SETTINGS'=>'Настройки',
     'NAV_CREATE_ITEM'=>'Создать страницу',
     'NAV_APPLICATION_CATEGORIES'=>'Категории',
     'NAV_CREATE_CATEGORY'=>'Создать категорию',
     'NAV_APPLICATION_ELEMENTS'=>'Элементы',
     'NAV_CREATE_ELEMENT'=>'Создать элемент',
+
+    'APPLICATIONS_CONTENTS_TITLE'=>'Контент главных страниц раздела',
 
 
     'LABEL_ELEMENT_NAME'=>'Наименование',
@@ -19,10 +22,12 @@ return [
     'LABEL_ELEMENT_META_TITLE'=>'Meta Title',
     'LABEL_ELEMENT_META_DESCRIPTION'=>'Meta Description',
     'LABEL_ELEMENT_META_KEYWORDS'=>'Meta Keywords',
+    'LABEL_ELEMENT_CATEGORY'=>'Категории страницы и основная категория',
 
     'ELEMENTS_UPDATE_SUCCESS'=>'Элемент успешно обновлён',
 
     'UPLOAD_FILE'=>'Перетащите изображение в эту область или нажмите',
     'SELECT_FILE'=>'выбрать на диске',
 
+    'ITEMS_LANGUAGE'=>'Язык',
 ];
