b27c661aae70d668bcf16dbe3e5374563ab19d11
Eugene Zakirov <worstinme@gmail.com>
2018-05-29 13:10:51 +0800

    string element

---
+++ elements/string/Element.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace worstinme\zoo\elements\string;
+
+class Element extends \worstinme\zoo\elements\BaseElement
+{
+    public $defaultStore = 'value_string';
+    public $itemsStore = 'varchar(255)';
+} 
