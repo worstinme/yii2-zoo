ca4a8957e5f3cf6c2ef8b187a711b2f2e3636528
Eugene Zakirov <worstinme@gmail.com>
2018-07-08 20:55:40 +0800

    money default step

--- elements/money/form.php
+++ elements/money/form.php
@@ -2,4 +2,4 @@
 
 use yii\helpers\Html;
 
-echo Html::activeInput('number', $model, $element->attributeName,['class'=>'uk-input']);
+echo Html::activeInput('number', $model, $element->attributeName,['class'=>'uk-input','step'=>'0.01']);
